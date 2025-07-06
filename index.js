e50e868a876b03e535ac2f44da29c59674bcdfa9
Tej ⛤ <165182589+tejvz@users.noreply.github.com>
2025-07-06 10:24:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Hi Skidders!
 // Welcome to Editable RPC!!!
-// Written with ðŸ§  by GhoSty | Publiced By Tejvz
+// Written with 🧠 by GhoSty | Publiced By Tejvz
 // Feel free to edit this RPC! But Give Credits
 const Discord = require('discord.js-selfbot-v13');
 const fs = require('fs');
@@ -8,106 +8,150 @@ const client = new Discord.Client({
   readyStatus: false,
   checkUpdate: false
 });
-// Editable RPC concept by GhoSty || Brutality
+
 const keepAlive = require('./server.js');
 keepAlive();
-// Editable RPC concept by GhoSty || Brutality
+
 let r;
 let startTimestamp = Date.now();
-let buttonName = 'Github ⛦';
+let buttonName = 'Github';
 let buttonUrl = 'https://github.com/tejvz';
 let rpcURL = 'https://dsc.gg/tejvz';
-let rpcType = 'PLAYING'; // COMPETING, WATCHING, PLAYING, STREAMING
-let rpcName = 'Tejv ⛦';
-let rpcDetails = 'dsc.gg/tejvz';
-let assetsSmallText = 'on Top!';
-let assetsLargeText = 'dsc.gg/tejvz';
-let assetsLargeImage = 'https://cdn.discordapp.com/attachments/1211178935405248513/1390681078985134172/standard.gif?ex=686b1e6e&is=6869ccee&hm=61aaf88d0afa0dbcb886c2ced262f0802f9a28674819dc15989f377dc87699a7&';
-let assetsSmallImage = 'https://cdn.discordapp.com/attachments/1211178935405248513/1390656444126462073/bl-cp.gif?ex=686b077d&is=6869b5fd&hm=833c477f69d8bf9e9a53ff1bd7732563d8c164ec8ddb0d90bf051221df75000e&';
-// Editable RPC concept by GhoSty || Brutality
+let rpcType = 'WATCHING'; // COMPETING, WATCHING, PLAYING, STREAMING
+let rpcName = 'Tejvz †';
+let rpcDetails = 'Dsc.gg/tejvz';
+let assetsSmallText = '24/7';
+let assetsLargeText = 'On Top!';
+let assetsLargeImage = 'https://media.discordapp.net/attachments/916987835230334996/1189819267546365972/17037453741388502409509294827371.gif?ex=6863820b&is=6862308b&hm=8d434ca39459de31c0225de588003b1aa1d8d45253663545cf6c738e6db2580f&';
+let assetsSmallImage = 'https://cdn.discordapp.com/attachments/916987835230334996/1389191480643424367/standard.gif?ex=6863b8e2&is=68626762&hm=e31079808f0008d5e8ba8cec681675aad48cf7785f120757cd7956660bce36c4&';
+
 client.on('ready', async () => {
   console.clear();
   console.log(`${client.user.tag} - rich presence started!`);
-  // Editable RPC concept by GhoSty || Brutality
-  const timestamps = fs.readFileSync('tejvz.txt', 'utf8').split('\n');
+
+  const timestamps = fs.readFileSync('ghosty.txt', 'utf8').split('\n');
   const randomTimestamp = timestamps[Math.floor(Math.random() * timestamps.length)];
   const [hours, minutes, seconds] = randomTimestamp.split(':');
   startTimestamp = Date.now() - (hours * 3600000 + minutes * 60000 + seconds * 1000);
-  // Editable RPC concept by GhoSty || Brutality
+
   updateRichPresence();
-  // Editable RPC concept by GhoSty || Brutality
   client.user.setPresence({ status: "dnd" });
 });
-// Editable RPC concept by GhoSty || Brutality
+
 client.on('message', message => {
   if (message.author.id === client.user.id) {
     const args = message.content.split(' ');
     const command = args[0];
-    // Editable RPC concept by GhoSty || Brutality
+
     if (command === '>simg' && args.length === 2) {
       const imgUrl = args[1];
       r.setAssetsSmallImage(imgUrl);
       client.user.setActivity(r);
       message.delete();
+
     } else if (command === '>time' && args.length === 2) {
       const newTime = args[1];
-      // Editable RPC concept by GhoSty || Brutality
       if (isValidTimestamp(newTime)) {
         const [hours, minutes, seconds] = newTime.split(':');
         startTimestamp = Date.now() - (hours * 3600000 + minutes * 60000 + seconds * 1000);
-        // Editable RPC concept by GhoSty || Brutality
         updateRichPresence();
         message.delete();
       } else {
         message.edit('Invalid timestamp format. Use HH:MM:SS.');
-      }// Editable RPC concept by GhoSty || Brutality
+      }
+
     } else if (command === '>bname' && args.length === 2) {
       buttonName = args[1];
       updateRichPresence();
       message.delete();
+
     } else if (command === '>burl' && args.length === 2) {
       buttonUrl = args[1];
       updateRichPresence();
       message.delete();
+
     } else if (command === '>rpcurl' && args.length === 2) {
       rpcURL = args[1];
       updateRichPresence();
       message.delete();
+
     } else if (command === '>rpctype' && args.length === 2) {
       rpcType = args[1];
       updateRichPresence();
       message.delete();
+
     } else if (command === '>rpcname' && args.length === 2) {
       rpcName = args[1];
       updateRichPresence();
       message.delete();
+
     } else if (command === '>rpcd' && args.length === 2) {
       rpcDetails = args.slice(1).join(' ');
       updateRichPresence();
       message.delete();
+
     } else if (command === '>stext' && args.length === 2) {
       assetsSmallText = args[1];
       updateRichPresence();
       message.delete();
+
     } else if (command === '>ltext' && args.length === 2) {
       assetsLargeText = args[1];
       updateRichPresence();
       message.delete();
+
     } else if (command === '>limg' && args.length === 2) {
       assetsLargeImage = args[1];
       updateRichPresence();
       message.delete();
+
+    } else if (command === '>rpch') {
+      message.delete();
+      message.channel.send(
+        "```" +
+`>limg: Changes the Large RPC image
+Usage: >limg IMAGE_URL
+
+>simg: Changes the Small RPC image
+Usage: >simg IMAGE_URL
+
+>time: Changes the RPC elapsed time
+Usage: >time TIME [For Example - >time 20:20:20]
+
+>bname: Changes the RPC Button Name
+Usage: >bname NAME
+
+>burl: Changes the RPC Button Url
+Usage: >burl URL
+
+>rpcurl: Changes the RPC Url [For Streaming Status Only]
+Usage: >rpcurl URL [For Example - >rpcurl https://link]
+
+>rpctype: Changes the RPC Type
+Usage: >rpctype TYPE [For Example - >rpctype STREAMING, WATCHING, COMPETING]
+
+>rpcname: Changes the RPC Name
+Usage: >rpcname NAME
+
+>rpcd: Changes the RPC Details
+Usage: >rpcd DETAILS
+
+>stext: Changes the Small Text
+Usage: >stext TEXT
+
+>ltext: Changes the Large Text
+Usage: >ltext TEXTL` +
+        "```"
+      );
     }
-    
-    // Editable RPC concept by GhoSty || Brutality
   }
 });
-// Editable RPC concept by GhoSty || Brutality
+
 function isValidTimestamp(timestamp) {
   const regex = /^\d{2}:\d{2}:\d{2}$/;
   return regex.test(timestamp);
-}// Editable RPC concept by GhoSty || Brutality
-// Editable RPC concept by GhoSty || Brutality
+}
+
 function updateRichPresence() {
   r = new Discord.RichPresence()
     .setApplicationId('1113183256343482519')
@@ -116,25 +160,15 @@ function updateRichPresence() {
     .setName(rpcName)
     .setDetails(rpcDetails)
     .setStartTimestamp(startTimestamp)
-    .setAssetsLargeImage(assetsLargeImage) 
-    .setAssetsLargeText(assetsLargeText) 
+    .setAssetsLargeImage(assetsLargeImage)
+    .setAssetsLargeText(assetsLargeText)
     .setAssetsSmallImage(assetsSmallImage)
     .setAssetsSmallText(assetsSmallText)
     .setButtons([
       { name: buttonName, url: buttonUrl }
-    ]);// Editable RPC concept by GhoSty || Brutality
-  // Editable RPC concept by GhoSty || Brutality
+    ]);
+
   client.user.setActivity(r);
 }
 
 client.login(process.env.TOKEN);
-
-// Editable RPC concept by GhoSty || Brutality
-// Editable RPC concept by GhoSty || Brutality
-// Editable RPC concept by GhoSty || Brutality
-// Editable RPC concept by GhoSty || Brutality
-// Editable RPC concept by GhoSty || Brutality
-// Editable RPC concept by GhoSty || Brutality
-// Editable RPC concept by GhoSty || Brutality
-// Editable RPC concept by GhoSty || Brutality
-// Editable RPC concept by GhoSty || Brutality
